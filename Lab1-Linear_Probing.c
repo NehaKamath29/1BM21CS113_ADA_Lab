67f4740e07d0dda30eac354577157bf479a3ad95
NehaKamath29 <117371852+NehaKamath29@users.noreply.github.com>
2023-06-17 12:08:18 +0530

    Add files via upload

---
+++ Lab1-Linear_Probing.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include<stdbool.h>
+#define size 11
+void main()
+{
+    int arr[]={11,12,4,97,68,41,13,44,51,61,24};
+    int hashtable[size];
+    bool table[size];
+    int i,j=0,index;
+    for(i=0;i<size;i++)
+    {
+        table[i]=false;
+    }
+    for(i=0;i<size;i++)
+    {
+        while(j<size)
+        {
+            index=(arr[i]+j)%size;
+            if(table[index]==false)
+            {
+                hashtable[index]=arr[i];
+                table[index]=true;
+                j=0;
+                break;
+            }
+            else
+            j++;
+        }
+    }
+    for(i=0;i<size;i++)
+    {
+        printf("%d ",hashtable[i]);
+    }
+}
