9e7726803a55be72ef44a6a4a05213939a5950c0
NehaKamath29 <117371852+NehaKamath29@users.noreply.github.com>
2023-08-05 12:09:13 +0530

    Add files via upload

---
+++ Lab6-Flyods.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+void main()
+{
+   int n,i,j,k;
+   printf("Enter the number of vertices:\n");
+   scanf("%d",&n);
+   int graph[n][n];
+   printf("Enter the cost matrix of the graph:\n");
+   for(i=0;i<n;i++)
+   {
+      for(j=0;j<n;j++)
+      {
+         scanf("%d",&graph[i][j]);
+      }
+   }
+   for(k=0;k<n;k++)
+   {
+      for(i=0;i<n;i++)
+      {
+         for(j=0;j<n;j++)
+         {
+            if(graph[i][j]>graph[i][k]+graph[k][j])
+            graph[i][j]=graph[i][k]+graph[k][j];
+         }
+      }
+   }
+   for(i=0;i<n;i++)
+   {
+      for(j=0;j<n;j++)
+      {
+         printf("%d   ",graph[i][j]);
+      }
+      printf("\n");
+   }
+
+}
